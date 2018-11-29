659774d98be688e769363d5864fc7e09517c370f
Arunkumar1992 <arunkumareee7@gmail.com>
2018-11-29 18:17:06 +0530

    Update ZREF.js

--- ZREF.js
+++ ZREF.js
@@ -2,6 +2,6 @@ console.log("Customer :: {{ order_number }} ");
 console.log("Customer :: {{customer.email}}) ");
 
 console.log("Order :: {{ total_price | money_without_currency }}" );
-
-var cemail= "{{customer.email}"; 
+var emailadd = {{customer.email}}
+var cemail= "{{customer.email}}"; 
 alert(cemail);
